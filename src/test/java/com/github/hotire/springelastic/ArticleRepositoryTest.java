cf51d6f3c68c59fe56cbd19ce4a7f62e0d2786ba
JangKunil <jangkeya@gmail.com>
2023-03-24 00:06:39 +0900

    edit ElasticsearchTemplate and Test

--- src/test/java/com/github/hotire/springelastic/ArticleRepositoryTest.java
+++ src/test/java/com/github/hotire/springelastic/ArticleRepositoryTest.java
@@ -1,21 +1,38 @@
 package com.github.hotire.springelastic;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
 @SpringBootTest
-public class ArticleRepositoryTest {
+class ArticleRepositoryTest {
 
     @Autowired
     private ArticleRepository articleRepository;
 
+    @Autowired
+    private ElasticsearchTemplate esTemplate;
+
+    @BeforeEach
+    void config() {
+        esTemplate.deleteIndex(Article.class);
+        esTemplate.createIndex(Article.class);
+        esTemplate.putMapping(Article.class);
+        esTemplate.refresh(Article.class);
+
+        articleRepository.save(new Article("test"));
+    }
+
     @Test
-    public void save() {
+    void save() {
         // given
         final String title = "hello";
         final Article article = new Article(title);
@@ -28,5 +45,18 @@ public class ArticleRepositoryTest {
         assertThat(saved.getTitle()).isEqualTo(title);
     }
 
+
+    @Test
+    void find() {
+        // given
+        final String title = "test";
+
+        // when
+        final List<Article> result = articleRepository.findAllByTitle(title);
+
+        // then
+       assertThat(result.size()).isEqualTo(1);
+       assertThat(result.get(0).getTitle()).isEqualTo(title);
+    }
 
 }
